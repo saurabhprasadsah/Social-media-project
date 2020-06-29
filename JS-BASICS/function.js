1e17eae6c0d43edd347a1331779282b2205c3fdd
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-06-29 16:30:21 +0530

    function.js

--- JS-BASICS/function.js
+++ JS-BASICS/function.js
@@ -8,3 +8,12 @@ function alpha(){
 function beta(){
     return "b"
 }
+
+function area(height, width){
+    if(typeof width === 'undefined'){
+        return Math.PI * height * height
+    }
+    return height * width
+}
+console.log('area 3,4',area(3,4))
+console.log('area 4',area(4))
