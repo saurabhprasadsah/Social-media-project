01f7d3bc1724e5c5b7c135ad13c99c08e629438a
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2023-08-12 22:02:09 +0530

    function.js

--- JS-BASICS/function.js
+++ JS-BASICS/function.js
@@ -1,28 +1,21 @@
-
 console.log(beta())
-
 function alpha(){
     return "A"
 }
-
 function beta(){
     return "b"
 }
-
 function area(height, width){
     if(typeof width === 'undefined'){
         return Math.PI * height * height
-    }
+}
     return height * width
 }
 console.log('area 3,4',area(3,4))
 console.log('area 4',area(4))
-
 function hello (){
-
     let arguments = "asdsjnf"
     console.log('hello world' + arguments[0] + arguments[1])
 }
 hello(1,2)
 hello('Arnav' , 'gupta')
-
