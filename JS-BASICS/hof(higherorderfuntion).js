2bf45dd330af92eacda5c6ad4703b178d25da877
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2023-08-12 21:59:11 +0530

    Update hof(higherorderfuntion).js

--- JS-BASICS/hof(higherorderfuntion).js
+++ JS-BASICS/hof(higherorderfuntion).js
@@ -1,27 +1,16 @@
 function createGreeter(greeting) {
-    
     function greet(name){
         console.log(greeting, name)
     }
-    return greet
-    
+    return greet    
 }
 
 function getName() {
     return document.getElementById('namebox').Value
 }
-
-
-
-
-
 let g1 = createGreeter('Goodmorning')
 let g2 = createGreeter('Good evening')
-
 console.log(typeof g1)
-
 console.log(g1('Arnva'))
 console.log(g2('prateek'))
-
-
 console.log(('saurabh'))
